4c7f8be75cd072c101ab85284e46305fb5e3a7df
槿瑟”git config --global user.email jinse.zjw@alibaba-inc.com” <jinse.zjw@alibaba-inc.com>
2014-07-11 16:24:18 +0800

    codereview

--- 1.0/build/mini.js
+++ 1.0/build/mini.js
@@ -153,7 +153,7 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
 		layer: {
 			value: {
                     tip : '其实...亲有更好的选择',
-                    btn_type_pic : 'http://gtms04.alicdn.com/tps/i4/TB1kLVqFVXXXXX1XpXXJPIyFpXX-143-56.png',
+                    btn_type_pic : 'http://gtms03.alicdn.com/tps/i3/TB1RcVQFVXXXXbvXFXXcgZpFXXX-140-48.png',
                     imgsrc : [ 'http://gtms01.alicdn.com/tps/i1/TB1UpxsFVXXXXbIXXXXIXul4XXX-860-342.png',
                                'http://gtms02.alicdn.com/tps/i2/TB1d9prFVXXXXc1XXXXl0Cl4XXX-860-343.png' ,
                                'http://gtms03.alicdn.com/tps/i3/TB1OORrFVXXXXcbXXXXIXul4XXX-860-342.png']
@@ -173,7 +173,7 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
 			}
 		},
 		updateLink: {
-			value: 'http://windows.microsoft.com/zh-cn/internet-explorer/download-ie',
+			value: 'http://windows.microsoft.com/zh-cn/windows/downloads',
 			setter: function(v){
 				return v;
 			}			
@@ -189,8 +189,7 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
 		 show:function(){
 		 	var self = this;
 			//	Storage.clear();
-		    self._uaTest(Storage);
-		 	
+			S.ready(self._uaTest(Storage));
 		 },
         /**
          * 运行
@@ -198,14 +197,11 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
          */
 		 render:function(Storage){
 			 	var self = this;
-
 			 	var html1 = self._getHtml(Storage);
-			 	if(html1 == "err")
-			 		return;
 			 	var supernatant =  $(html1);
 			 	supernatant.prependTo("body");
 
-				if(Storage.get("tipBar")==1 && !self._promptIE(Storage)){
+				if(Storage.get("tipBar")==1 && !self._shouldLayerPrompt(Storage)){
 			   		var tipEl =S.get('#pupUplayer_tipel')
 					DOM.show(tipEl);
 				    var anim = new Anim(tipEl, {
@@ -215,71 +211,77 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
 			    }else{
 					S.use('gallery/slide/1.3/index', function(S,Slide){
 						Storage.remove("tipBar");
-						S.ready(function(S){
-								DOM.style("#down","height",DOM.docHeight());
-								var viewPortHeight = DOM.viewportHeight();
-								var verticalHeight = DOM.viewportHeight()/2-266;
-								DOM.style("#pupUplayer .pUl_container","marginTop",verticalHeight+"px");		
-								DOM.style("#pupUplayer .pUl_container","display","block");
+						DOM.style("#down","height",DOM.docHeight());
+						var viewPortHeight = DOM.viewportHeight();
+						var verticalHeight = DOM.viewportHeight()/2-266;
+						var pULContainer = S.all("#pupUplayer .pUl_container");
+						DOM.style(pULContainer,"marginTop",verticalHeight+"px");		
+						DOM.style(pULContainer,"display","block");
 
-								C = new Slide('slides',{
-									autoSlide:false,
-									hoverStop:true,
-									effect:'hSlide',
-									timeout:2000,
-									speed:400,
-									invisibleStop:true,
-									eventType:'click',
-									triggerDelay:200,
-									defaultTab:0,
-									selectedClass:'current',
-									carousel:true,
-									touchmove:true
-								});
+						C = new Slide('slides',{
+							autoSlide:true,
+							hoverStop:true,
+							effect:'hSlide',
+							timeout:1300,
+							speed:400,
+							invisibleStop:true,
+							eventType:'click',
+							triggerDelay:1000,
+							defaultTab:0,
+							selectedClass:'current',
+							carousel:true
 						});
-						S.all("#pupUplayer").delegate('click','.prev',function(e){
-								e.halt();
-								C.previous();
-								if(C.autoSlide && C.stoped === false){
-									C.stop().play();
-								}
-						});
-						S.all("#pupUplayer").delegate('click','.next',function(e){
-								e.halt();
-								C.next();
-								if(C.autoSlide && C.stoped === false){
-									C.stop().play();
-								}
-						});
-						S.all("#pupUplayer").delegate('mouseenter','#pupUplayer .closebtnspan',function(){
-								DOM.style("#pupUplayer .down-close","visibility","visible");
-						});
-						S.all("#pupUplayer").delegate('mouseleave','#pupUplayer .down-close',function(){
-								DOM.style("#pupUplayer .down-close","visibility","hidden");
-						});
-						S.all("#pupUplayer").delegate('click','#pupUplayer .down_close_btn',function(){
-								Storage.set("timeStamp",new Date().getTime());
-								DOM.style("#pupUplayer","display","none");
-								if(self.get('toptipBar').enable == true){
-									Storage.set("tipBar",1);
-									var tipEl =S.get('#pupUplayer_tipel')
-									DOM.show(tipEl);
-								    var anim = new Anim(tipEl, {
-								       height: 45
-								     }, .3, "easeOut");
-								    return anim.run();
-								}
-						});
+						if(self.get('layer').imgsrc.length == 1){
+							DOM.style("#pupUplayer .pagination","visibility","hidden");
+							$("#pupUplayer .next span").hide();
+							$("#pupUplayer .prev span").hide();
+						}
+					self._EventDelegate();
+
 					});
 				}
+		 },
+		 _EventDelegate : function(){
+		 		var self = this;
+		 		var pupUplayer = S.all("#pupUplayer");
+				var downClose = S.all("#pupUplayer .down-close");
+				pupUplayer.delegate('click','.prev',function(e){
+						e.halt();
+						C.previous();
+						if(C.autoSlide && C.stoped === false){
+							C.stop().play();
+						}
+				});
+				pupUplayer.delegate('click','.next',function(e){
+						e.halt();
+						C.next();
+						if(C.autoSlide && C.stoped === false){
+							C.stop().play();
+						}
+				});
+				pupUplayer.delegate('mouseenter','.closebtnspan',function(){
+						DOM.style(downClose,"visibility","visible");
+				});
+				pupUplayer.delegate('mouseleave','.down-close',function(){
+						DOM.style(downClose,"visibility","hidden");
+				});
+				pupUplayer.delegate('click','.down_close_btn',function(){
+						Storage.set("timeStamp",new Date().getTime());
+						DOM.style("#pupUplayer","display","none");
+						if(self.get('toptipBar').enable == true){
+							Storage.set("tipBar",1);
+							var tipEl =S.get('#pupUplayer_tipel')
+							DOM.show(tipEl);
+								var anim = new Anim(tipEl, {
+								    height: 45
+								}, .3, "easeOut");
+							return anim.run();
+						}
+				});
 		 },
 		 _getHtml : function(Storage){
 			 	var self = this;
 			 	var imageCount = self.get('layer').imgsrc.length;
-			 	if(imageCount<3 || imageCount>5){
-			 		alert("轮播数量建议在3-5张");
-			 		return "err";
-			 	}
 			 	var slideImg = '';
 			 	var slideLast = '';
 			 	for(var i=0;i<imageCount;i++) { 
@@ -337,62 +339,38 @@ KISSY.add('gallery/DepartureLayer/1.0/index',function(S,CORE,UA,Anim,Storage) {
 					<div id="down"></div>\
 				<div>';
 
-	      		if (self._promptIE(Storage)){
+	      		if (self._shouldLayerPrompt(Storage)){
 					return supernatantHtml;
 				}else if(Storage.get("tipBar")==1){
 					return supernatantTipBarHtml;
 				}
 		 },
 		 /**
-         * 判断浏览器类型
+         * 判断浏览器版本是否符合要求
          */
-        _find : function(Storage, type, version){
-        	var self = this;
-				switch(type){
-	            	case 'chrome': 
-	            		if(UA.chrome <= version && (self._promptIE(Storage) || Storage.get("tipBar")==1) ){
-	            			return true;
-	            			// self.render(Storage);
-	            		} break;
-	            	case 'safari':
-	            		if(UA.safari <= version && (self._promptIE(Storage) || Storage.get("tipBar")==1) ){
-	            			return true;
-	            		} break;
-	            	case 'opera':
-	            		if(UA.opera <= version && (self._promptIE(Storage) || Storage.get("tipBar")==1) ){
-	            			return true;
-	            		} break;
-	            	case 'firefox':
-	            		if(UA.firefox <= version && (self._promptIE(Storage) || Storage.get("tipBar")==1) ){
-	            			return true;
-	            		} break;
-	            	case 'ie':
-	            		if(UA.ie <= version && (self._promptIE(Storage) || Storage.get("tipBar")==1) ){
-	            			return true;
-	            		} break;	
-	            	default:
-	            		return false;
-	            }
+        _checkVersion : function(Storage, type, version){
+         	var self = this;
+	   		if(!UA[type])		return false;          
+	        else
+	        	return UA[type] <= version;
         },
         _uaTest : function(Storage){
             var self = this, count = self.get('browser').length, flag=false, i=0;
-            while(!self._find(Storage, self.get('browser')[i].browser, self.get('browser')[i].maxversion)){
-            	i++;
-            	if(i == count)	break;
-            }
-            if(i < count)	self.render(Storage);
+
+            S.each(self.get('browser'),function(b){
+            	if(self._checkVersion(Storage, b.browser, b.maxversion))
+            		self.render(Storage);
+            });
+            return false;
 	    },
 	    /**
-         * IE的话，判断是否关闭超过一周
+         *  判断浮层距离上次关闭时间是否超过 intervalTime
          */
-        _promptIE : function(Storage){
+        _shouldLayerPrompt : function(Storage){
         	var self = this;
             var o = Storage.get("timeStamp");
             var isMoreThanWeek = (new Date().getTime() - parseInt(o, 10)) > self.get('intervalTime');  
-            if (!o || isMoreThanWeek)
-            	return true;
-            else
-            	return false;
+            return (!o || isMoreThanWeek);
         },
 		_mymix : function(defaultAttr, comconfig){
 			var self = this;
